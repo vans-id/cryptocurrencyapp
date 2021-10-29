a502fe5ae6cd2390aa2169223eb7e9c230fbea02
Evan <djevann11@gmail.com>
2021-10-29 10:24:33 +0700

    setup project

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/data/remote/CoinPaprikaApi.kt
@@ -0,0 +1,4 @@
+package com.plcoding.cryptocurrencyappyt.data.remote
+
+interface CoinPaprikaApi {
+}
---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.plcoding.cryptocurrencyappyt.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkGray = Color(0xFF16171D)
+val MediumGray = Color(0xFF1F222A)
+val ColorPrimary = Color(0xFFFFC412)
+val TextWhite = Color(0xFFEEEEEE)
